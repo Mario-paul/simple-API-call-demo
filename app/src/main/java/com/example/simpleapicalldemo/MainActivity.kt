1d3cc3be484b6660bb64c448a85044c763e78019
Mario-paul <15961362+Mario-paul@users.noreply.github.com>
2022-10-21 15:59:53 -0400

    212: create app that downloads a text website
    - app does an API call to download a website in JSON format
    - add custom progress dialog
    - uses deprecated methods (AsyncTask and its override methods)

--- app/src/main/java/com/example/simpleapicalldemo/MainActivity.kt
+++ app/src/main/java/com/example/simpleapicalldemo/MainActivity.kt
@@ -1,11 +1,112 @@
 package com.example.simpleapicalldemo
 
+import android.app.Dialog
+import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import java.io.BufferedReader
+import java.io.IOException
+import java.io.InputStreamReader
+import java.net.HttpURLConnection
+import java.net.SocketTimeoutException
+import java.net.URL
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        CallAPILoginAsyncTask().execute() // call our custom class
+
     }
+
+    //
+    private inner class CallAPILoginAsyncTask() : AsyncTask<Any, Void, String>() {
+
+        private lateinit var customProgressDialog: Dialog
+
+        override fun onPreExecute() {
+            super.onPreExecute()
+
+            showProgressDialog()
+        }
+
+        override fun doInBackground(vararg params: Any?): String {
+
+            var result: String
+            var connection: HttpURLConnection? = null
+
+            try {
+
+                val url = URL("https://run.mocky.io/v3/cf6e606a-6e63-4bf4-9a6e-b10dfeaf15db")
+                // secret delete link -- warning, clicking this link will delete the above Mocky:
+                // https://designer.mocky.io/manage/delete/cf6e606a-6e63-4bf4-9a6e-b10dfeaf15db/LPjwBvWmFYf5xSpUCdq778Z9dq4jpmzPQWnb
+
+                connection = url.openConnection() as HttpURLConnection
+                connection.doInput = true
+                connection.doOutput = true
+
+                val httpResult: Int = connection.responseCode
+
+                if (httpResult == HttpURLConnection.HTTP_OK) {
+
+                    val inputStream = connection.inputStream
+
+                    val reader = BufferedReader(InputStreamReader(inputStream))
+                    val stringBuilder = StringBuilder()
+                    var line: String?
+
+                    try {
+                        while (reader.readLine().also { line = it } != null) {
+                            stringBuilder.append(line + "\n")
+                        }
+                    } catch (e: IOException) {
+                        e.printStackTrace()
+                    } finally {
+                        try {
+                            inputStream.close()
+                        } catch (e: IOException) {
+                            e.printStackTrace()
+                        }
+                    }
+
+                    result = stringBuilder.toString()
+
+                } else {
+                    result = connection.responseMessage
+                }
+
+            } catch (e: SocketTimeoutException) {
+                result = "Error: Connection Timeout"
+            } catch (e: Exception) {
+                result = "Error: " + e.message
+            } finally {
+                connection?.disconnect()
+            }
+
+            return result
+
+        }
+
+        override fun onPostExecute(result: String?) {
+            super.onPostExecute(result)
+
+            cancelProgressDialog()
+
+            Log.i("JSON RESPONSE RESULT", result!!)
+        }
+
+        private fun showProgressDialog() {
+            customProgressDialog = Dialog(this@MainActivity)
+            customProgressDialog.setContentView(R.layout.dialog_custom_progress)
+            customProgressDialog.show()
+        }
+
+        private fun cancelProgressDialog() {
+            customProgressDialog.dismiss()
+        }
+
+    }
+
 }
